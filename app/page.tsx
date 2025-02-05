409f4abb254fcd1a6b7e1704a8b33be219b3e73c
Fernando Jauya <fernandojauya30@gmail.com>
2025-02-05 13:56:36 -0500

    🐛 Fix: Envolver useSearchParams en Suspense para evitar error en SSG

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { Suspense } from "react";
 import ImageGallery from "./_components/ImageGallery";
 export default function Home() {
   return (
     <div className="flex gap-3 items-center px-5 py-3">
-      <ImageGallery />
+      <Suspense fallback={<p>Cargando...</p>}>
+        <ImageGallery />
+      </Suspense>
     </div>
   );
 }
